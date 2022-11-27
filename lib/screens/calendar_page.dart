547c6b495a242be9c02f305f87ddbb0372f46e1e
sa8a <jpkrllll@yahoo.co.jp>
2022-11-27 22:31:15 +0900

    add: カレンダーを表示

--- lib/screens/calendar_page.dart
+++ lib/screens/calendar_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:table_calendar/table_calendar.dart';
 
 class CalendarPage extends StatelessWidget {
   const CalendarPage({Key? key}) : super(key: key);
@@ -9,8 +10,12 @@ class CalendarPage extends StatelessWidget {
       appBar: AppBar(
         title: const Text('腹痛管理'),
       ),
-      body: const Center(
-        child: Text('カレンダーを表示'),
+      body: Center(
+        child: TableCalendar(
+          firstDay: DateTime(2022, 11, 27),
+          lastDay: DateTime(2040, 12, 31),
+          focusedDay: DateTime.now(),
+        ),
       ),
     );
   }
