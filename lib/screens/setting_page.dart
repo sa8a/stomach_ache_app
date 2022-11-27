0847f2982112a5c09283ec0b470ea0bb0148e9e5
sa8a <jpkrllll@yahoo.co.jp>
2022-11-27 22:23:58 +0900

    add: 設定画面をListViewで表示

--- lib/screens/setting_page.dart
+++ lib/screens/setting_page.dart
@@ -9,8 +9,16 @@ class SettingPage extends StatelessWidget {
       appBar: AppBar(
         title: const Text('設定'),
       ),
-      body: const Center(
-        child: Text('設定を表示'),
+      body: Center(
+        child: ListView(
+          children: const [
+            ListTile(
+              leading: Icon(Icons.share),
+              title: Text('このアプリをシェア'),
+              trailing: Icon(Icons.keyboard_arrow_right_rounded),
+            ),
+          ],
+        ),
       ),
     );
   }
