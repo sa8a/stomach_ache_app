6b40bddea3008e2bc7348b0b5aa847ceeaa278db
sa8a <jpkrllll@yahoo.co.jp>
2022-11-29 18:13:25 +0900

    add: カレンダーのイベントを読み込み

--- lib/screens/calendar_page.dart
+++ lib/screens/calendar_page.dart
@@ -1,3 +1,4 @@
+import 'dart:collection';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 import 'package:table_calendar/table_calendar.dart';
@@ -26,8 +27,61 @@ class _CalendarPageState extends State<CalendarPage> {
   DateTime _focusedDay = DateTime.now();
   DateTime? _selectedDay;
 
+  // イベントリスト
+  Map<DateTime, List> _eventsList = {};
+
+  int getHashCode(DateTime key) {
+    return key.day * 1000000 + key.month * 10000 + key.year;
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _selectedDay = _focusedDay;
+    //サンプルのイベントリスト
+    _eventsList = {
+      DateTime.now().subtract(Duration(days: 2)): ['Event A6', 'Event B6'],
+      DateTime.now(): ['Event A7', 'Event B7', 'Event C7', 'Event D7'],
+      DateTime.now().add(Duration(days: 1)): [
+        'Event A8',
+        'Event B8',
+        'Event C8',
+        'Event D8'
+      ],
+      DateTime.now().add(Duration(days: 3)):
+          Set.from(['Event A9', 'Event A9', 'Event B9']).toList(),
+      DateTime.now().add(Duration(days: 7)): [
+        'Event A10',
+        'Event B10',
+        'Event C10'
+      ],
+      DateTime.now().add(Duration(days: 11)): ['Event A11', 'Event B11'],
+      DateTime.now().add(Duration(days: 17)): [
+        'Event A12',
+        'Event B12',
+        'Event C12',
+        'Event D12'
+      ],
+      DateTime.now().add(Duration(days: 22)): ['Event A13', 'Event B13'],
+      DateTime.now().add(Duration(days: 26)): [
+        'Event A14',
+        'Event B14',
+        'Event C14'
+      ],
+    };
+  }
+
   @override
   Widget build(BuildContext context) {
+    final _events = LinkedHashMap<DateTime, List>(
+      equals: isSameDay,
+      hashCode: getHashCode,
+    )..addAll(_eventsList);
+
+    List getEventForDay(DateTime day) {
+      return _events[day] ?? [];
+    }
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('腹痛管理'),
@@ -38,6 +92,7 @@ class _CalendarPageState extends State<CalendarPage> {
           firstDay: DateTime(2022, 1, 1),
           lastDay: DateTime(2040, 12, 31),
           focusedDay: _focusedDay,
+          eventLoader: getEventForDay,
 
           selectedDayPredicate: (day) {
             return isSameDay(_selectedDay, day);
@@ -49,6 +104,9 @@ class _CalendarPageState extends State<CalendarPage> {
                 _focusedDay = focusedDay;
               });
             }
+          },
+          onPageChanged: (focusedDay) {
+            _focusedDay = focusedDay;
           },
 
           // 曜日の日本語対応
