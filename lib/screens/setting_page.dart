f2667decf893bb6179ecee8749a1480d4ee69adf
sa8a <jpkrllll@yahoo.co.jp>
2022-12-15 00:53:50 +0900

    add: 削除時の確認ポップアップを追加 #14

--- lib/screens/setting_page.dart
+++ lib/screens/setting_page.dart
@@ -3,13 +3,11 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:share_plus/share_plus.dart';
 import 'package:stomach_ache_app/model/calendar.dart';
 
-class SettingPage extends ConsumerWidget {
+class SettingPage extends StatelessWidget {
   const SettingPage({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
-    final calendar = ref.watch(calendarProvider);
-
+  Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('設定'),
@@ -30,11 +28,46 @@ class SettingPage extends ConsumerWidget {
               title: const Text('カレンダーのデータを削除する'),
               trailing: const Icon(Icons.keyboard_arrow_right_rounded),
               onTap: () {
-                calendar.deleteCalendarEvent();
+                showDialog<void>(
+                  context: context,
+                  builder: (_) {
+                    return const DeleteAlertDialog();
+                  },
+                );
               },
             ),
           ],
         ),
+      ),
+    );
+  }
+}
+
+// 削除時のポップアップ
+class DeleteAlertDialog extends StatelessWidget {
+  const DeleteAlertDialog({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Consumer(
+      builder: (context, ref, child) => AlertDialog(
+        title: const Text('本当に削除しますか？'),
+        content: const Text('削除するとカレンダーのデータが全て消えます。\nこの変更は戻せません。'),
+        actions: [
+          TextButton(
+            child: const Text('いいえ'),
+            onPressed: () {
+              Navigator.pop(context);
+            },
+          ),
+          TextButton(
+            child: const Text('はい'),
+            onPressed: () {
+              ref.watch(calendarProvider).deleteCalendarEvent();
+              Navigator.pop(context);
+            },
+          )
+        ],
       ),
     );
   }
