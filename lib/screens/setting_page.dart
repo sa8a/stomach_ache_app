ffa7761582a23b18546837130104bd84cf776713
sa8a <jpkrllll@yahoo.co.jp>
2022-12-28 07:30:54 +0900

    remove: 設定のShere機能

--- lib/screens/setting_page.dart
+++ lib/screens/setting_page.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:share_plus/share_plus.dart';
 import 'package:stomach_ache_app/model/calendar.dart';
 import 'package:fluttertoast/fluttertoast.dart';
 
@@ -16,14 +15,6 @@ class SettingPage extends StatelessWidget {
       body: Center(
         child: ListView(
           children: [
-            ListTile(
-              leading: const Icon(Icons.share),
-              title: const Text('このアプリをシェア'),
-              trailing: const Icon(Icons.keyboard_arrow_right_rounded),
-              onTap: () {
-                Share.share('共有');
-              },
-            ),
             ListTile(
               leading: const Icon(Icons.delete),
               title: const Text('カレンダーのデータを削除する'),
