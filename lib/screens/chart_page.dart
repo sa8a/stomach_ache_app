f9a9f326f614a0674ce8883e12d3dfad213312b6
sa8a <jpkrllll@yahoo.co.jp>
2023-01-07 00:19:11 +0900

    add: サンプルPieChart

--- lib/screens/chart_page.dart
+++ lib/screens/chart_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:fl_chart/fl_chart.dart';
 
 class ChartPage extends StatelessWidget {
   const ChartPage({Key? key}) : super(key: key);
@@ -9,8 +10,27 @@ class ChartPage extends StatelessWidget {
       appBar: AppBar(
         title: const Text('グラフ'),
       ),
-      body: const Center(
-        child: Text('グラフ'),
+      body: Center(
+        child: PieChart(
+          PieChartData(
+            // 開始位置を12時方向にするためにはstartDegreeOffsetを指定
+            // デフォルトだと3時の方向なので、値は270や-90など開始位置から90度反時計回りに
+            startDegreeOffset: 270,
+            centerSpaceRadius: 50, // くり抜き
+            sectionsSpace: 0,
+            sections: [
+              PieChartSectionData(
+                borderSide: const BorderSide(color: Colors.black, width: 0),
+                color: Colors.red,
+                value: 2 / 24 * 100,
+                titlePositionPercentageOffset: 0.5,
+                title: "サンプル\nサンプル",
+                titleStyle: const TextStyle(fontSize: 16, color: Colors.white),
+                radius: 120,
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
